567762cd3684993bb398f2c77d3c76a274c6736c
Adriana <adriana.mendoza.b@ucb.edu.bo>
2024-08-16 18:32:33 -0400

    ac

--- calculadora.py
+++ calculadora.py
@@ -1,4 +1,26 @@
-nombre= (input ("ingrese numero"))
-print (nombre)
+def calculadora():
+    operacion = input("Ingresa la operación deseada (+, -, *, /): ")
 
-print (type(nombre))
+    num1 = float(input("Ingresa el primer número: "))
+    num2 = float(input("Ingresa el segundo número: "))
+
+    if operacion == '+':
+        resul = num1 + num2
+    elif operacion == '-':
+        resul = num1 - num2
+    elif operacion == '*':
+        resul = num1 * num2
+    elif operacion == '/':
+        if num2 == 0:
+            resul = "Error: No se puede dividir por cero."
+        else:
+            resul = num1 / num2
+    else:
+        resul = "Operación no válida. Usa +, -, * o /."
+
+    print(f"El resultado es: {result}")
+
+if __name__ == "__main__":
+    calculator()
+
+
