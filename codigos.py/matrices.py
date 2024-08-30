4775c0e351f0d889ea2ade347412284c79da3126
Adriana <adriana.mendoza.b@ucb.edu.bo>
2024-08-30 19:03:35 -0400

    clase30/8

---
+++ codigos.py/matrices.py
@@ -0,0 +1,15 @@
+lista [1, 2, 3, 4, 5] #unidimensional
+     #
+print(lista) # unidimesional 
+print (lista [3])
+matriz= [
+[1,2,3], 
+[4,5,6], 
+[7,8,9]] 
+print [matriz[1][2]]
+
+listastring=["a",2.2, "c", 10]
+print(type(listastring))#muestra que tipo de variable es 
+
+# entre comillas dobles es un dato str
+#sin comillas es int 
