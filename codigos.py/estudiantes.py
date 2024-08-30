dfb1c650f22fb18fb50a2dcb56f2aa6a93d1d8f7
Adriana <adriana.mendoza.b@ucb.edu.bo>
2024-08-30 19:20:48 -0400

    cc

---
+++ codigos.py/estudiantes.py
@@ -0,0 +1,12 @@
+estudiantes = int(input("Ingrese a cuantos estuadiantes quiere calificar: "))
+total = estudiantes
+suma = 0
+while estudiantes > 0:
+    nota = float(input())
+    suma = suma+nota
+    estudiantes -= 1
+promedio = suma/total
+print(promedio)
+
+estudiantes= int(input("Ingrese a cuantos estudiantes quiere calificar:"))
+estudiantes
