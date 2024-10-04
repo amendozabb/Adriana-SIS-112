ce5c4c4e864f53e6ce0273dea52611e884e94060
Adriana <adriana.mendoza.b@ucb.edu.bo>
2024-10-04 19:12:29 -0400

    lista

--- codigos.py/listafun.py
+++ codigos.py/listafun.py
@@ -4,12 +4,12 @@ def generarLista(tamaniolista,valorMinimo,valorMaximo,lista):
     for i in range(tamaniolista):
         numaleatorio= random.randint(valorMinimo,valorMaximo)
         lista.append(numaleatorio)
-        return (lista)  
+    return lista
 
 def encontrarLista(num,lista):
     for i in range(len(lista)):
         if num== lista[i]:
-            print(f"El numero encontrado esta en el indice:{i}")
+            print(f"El numero encontrado esta en el indice:{i}") 
     
 
      
