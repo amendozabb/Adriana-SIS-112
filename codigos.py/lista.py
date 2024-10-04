753bc2451c2e99529c27147409b7cbbdd39052fa
Adriana <adriana.mendoza.b@ucb.edu.bo>
2024-10-04 19:03:40 -0400

    tiempo y listas

---
+++ codigos.py/lista.py
@@ -0,0 +1,30 @@
+import listafun as lis
+import time
+listaNum=[]
+listComp= int(input("Ingrese la cantidad de datos para la lista:"))
+max= int(input("Ingrese el maximo numeros a generar:"))
+min=int(input("Ingrese el minimo numero a generar:"))
+
+#tiempo en generar lista
+inicio=time.time()
+
+lis.generarLista(listComp,min,max,listaNum) #se llama a la funcion del otro archivo
+print(listaNum)
+print(len(listaNum))
+print(type(listaNum))
+
+fin= time.time()
+print(f"Tiempo transcurrido de la generacion de la lista \n {(fin-inicio)*1000}ms")
+
+
+#funcion para encontar un numero
+inicio=time.time() #tiempo para encontrar el numero
+
+num=int(input("Ingrese el numero a encontrar:"))
+lis.encontrarLista(num, listaNum)
+
+fin= time.time()
+print(f"Tiempo transcurrido en encontrar el numero \n {(fin-inicio)*1000}ms") #fin del tiempo 
+
+
+
---
+++ codigos.py/listafun.py
@@ -0,0 +1,16 @@
+import random
+
+def generarLista(tamaniolista,valorMinimo,valorMaximo,lista):
+    for i in range(tamaniolista):
+        numaleatorio= random.randint(valorMinimo,valorMaximo)
+        lista.append(numaleatorio)
+        return (lista)  
+
+def encontrarLista(num,lista):
+    for i in range(len(lista)):
+        if num== lista[i]:
+            print(f"El numero encontrado esta en el indice:{i}")
+    
+
+     
+
